2b496b152f669a96a9b532883697db9c6ef516e5
Dhayanandhan <dhayaec@gmail.com>
2020-03-14 11:16:53 +0530

    fix eslint config

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 
 export default function Button(props: any) {
-    // eslint-disable-next-line react/jsx-props-no-spreading
     const { title } = props;
     return (
-        // eslint-disable-next-line react/jsx-props-no-spreading
         <button className="btn-blue" type="button" {...props}>
             {title}
         </button>
